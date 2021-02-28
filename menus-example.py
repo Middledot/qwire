c285ac3fbe34098d066ef61a1a1c71028f9510e3
MiddledotDev <78228142+MiddledotDev@users.noreply.github.com>
2021-02-28 09:29:46 -0500

    Update and rename reaction-moving-discord.py to menus-example.py

--- menus-example.py
+++ menus-example.py
@@ -5,63 +5,81 @@ from discord.ext import commands
 client = commands.Bot(command_prefix='?')
 
 @client.command()
-# the following checks if the bot can add reactions, throws an error if cannot
 @commands.bot_has_permissions(add_reactions=True)
-async def example(ctx):
-    # In the following list, set all the "pages" of which your bot will cycle through
-    listt = [discord.Embed(description="Example 1"), discord.Embed(description="Example 2"), discord.Embed(description="Example 3")]
+async def menus(ctx):
+    # Just edit the list of embeds for it to work
+    listt = [discord.Embed(description="Page 1"), discord.Embed(description="Page 2"), discord.Embed(description="Page 3"), discord.Embed(description="Page 4")]
     current = 0
-    # Sends the initial message
     msg = await ctx.send(embed=listt[0].set_footer(text=f"{str(current+1)}/{str(len(listt))}"))
-    # Adds all the reactions
     await msg.add_reaction('⏮️')
     await msg.add_reaction('◀️')
     await msg.add_reaction('▶️')
     await msg.add_reaction('⏭️')
+    await msg.add_reaction('🔢')
     await msg.add_reaction('⏹️')
     def check(reaction, user):
         return user == ctx.author and reaction.message.id == msg.id
-    # This forever loop is only broken when the user doesn't respond within 60 seconds or they pressed the ⏹️ option
     while True:
         try:
             reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
         except asyncio.TimeoutError:
             break
         else:
-            # This checks if the bot can remove the users reaction to make maneuvering faster for the user
             if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
                 await msg.remove_reaction(reaction, user)
-
-            # next
             if str(reaction) == '▶️':
                 if current != len(listt)-1:
                     await msg.edit(embed=listt[current+1].set_footer(text=f"{str(current+2)}/{str(len(listt))}"))
                     current = current + 1
-            # previous
             if str(reaction) == '◀️':
                 if current != 0:
                     await msg.edit(embed=listt[current-1].set_footer(text=f"{str(current)}/{str(len(listt))}"))
                     current = current - 1
-            # beginning
             if str(reaction) == '⏮️':
                 current = 0
                 await msg.edit(embed=listt[0].set_footer(text=f"{str(current+1)}/{str(len(listt))}"))
-            # last
             if str(reaction) == '⏭️':
                 current = len(listt)-1
                 await msg.edit(embed=listt[len(listt)-1].set_footer(text=f"{str(current+1)}/{str(len(listt))}"))
-            # stop
+            if str(reaction) == '🔢':
+                ms = await ctx.send('What page would you like to go to?')
+                def checkit(m):
+                    return m.author == ctx.author and m.channel == ctx.channel and m.guild == ctx.guild
+                try:
+                    mess = await client.wait_for('message', timeout=60.0, check=checkit)
+                except asyncio.TimeoutError:
+                    break
+                else:
+                    try:
+                        await ms.delete()
+                    except:
+                        pass
+                    if ctx.channel.permissions_for(ctx.guild.me).manage_messages:
+                        await mess.delete()
+                    try:
+                        t = int(mess.content)
+                    except:
+                        await ctx.send('Invalid page number.')
+                    else:
+                        if t < 0 or t > len(listt):
+                            await ctx.send('Invalid page number.')
+                        else:
+                            await msg.edit(embed=listt[t-1].set_footer(text=f"{str(t)}/{str(len(listt))}"))
+                            current = t-1
             if str(reaction) == '⏹️':
                 await msg.edit(embed=discord.Embed(description="Ended"))
                 await msg.remove_reaction('⏮️', client.user)
                 await msg.remove_reaction('◀️', client.user)
                 await msg.remove_reaction('▶️', client.user)
                 await msg.remove_reaction('⏭️', client.user)
+                await msg.remove_reaction('🔢', client.user)
                 await msg.remove_reaction('⏹️', client.user)
                 break
 @example.error
-async def exmaple_error(ctx, error):
+async def example_error(ctx, error):
     if isinstance(error, commands.BotMissingPermissions):
-        await ctx.send('Cannot initialize pages, missing permissions to add reactions.')
+        await ctx.send('❌ Do not know why but I do not have permission to add reactions.')
+    else:
+        raise error
 
-client.run(YOUR_TOKEN)
+client.run('YOUR_TOKEN')
